60bad6e92c294a9e3793f3ac71b20fb7d39efcb7
Wayne Duran <asartalo@gmail.com>
2019-05-06 11:29:42 +0800

    chore: Update release script

--- release.sh
+++ release.sh
@@ -1,7 +1,7 @@
 bundle exec gem bump $1 && \
   VERSION=$(ruby -e "require './lib/sugod/version.rb'; puts Sugod::VERSION") && \
   git-chglog --next-tag $VERSION -o CHANGELOG.md && \
-  git commit -am "Release CHANGELOG" && \
+  git commit -am "Release CHANGELOG for $VERSION" && \
   bundle exec gem tag && \
   rake release && \
   git push origin master
